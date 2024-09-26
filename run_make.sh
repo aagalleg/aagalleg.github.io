fc9ea3bbead72456eb88dc28dfa8d760ca0b9a1f
aagalleg <alberto.gallegos.muro@intel.com>
2024-09-26 10:16:38 -0700

    Next try

---
+++ run_make.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+make -C docs clean 
+# sphinx-apidoc -f -o docs/source . -H Test -e -t docs/source/_templates
+make -C docs html
+git config --global --add safe.directory docs/build/html
+cd docs/build/html/
+git init
+touch .nojekyll
+git add -A
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git commit -m 'deploy'
