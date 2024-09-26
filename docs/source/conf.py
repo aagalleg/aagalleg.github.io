7300c8cc19af443bd00a83124587c48b45cdfab8
aagalleg <alberto.gallegos.muro@intel.com>
2024-09-26 15:31:36 -0700

    New test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'TestingOP'
+project = 'TestOp'
 copyright = '2024, Alberto Gallegos'
 author = 'Alberto Gallegos'
 release = '0.1'
@@ -14,7 +14,10 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
+html_show_sourcelink = True
