d0d06b6e84fc685923b0003341625f066744085a
aagalleg <alberto.gallegos.muro@intel.com>
2024-09-26 10:28:07 -0700

    next try

--- run_make.sh
+++ run_make.sh
@@ -2,10 +2,9 @@
 make -C docs clean 
 # sphinx-apidoc -f -o docs/source . -H Test -e -t docs/source/_templates
 make -C docs html
-chown runner:docker -R *
-git config --global --add safe.directory docs/build/html
 cd docs/build/html/
 git init
+sh -c "git config --global --add safe.directory $PWD"
 touch .nojekyll
 git add -A
 git config --local user.email "action@github.com"
